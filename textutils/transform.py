048805f26bf3387baf50f139bd77cd8cf244b8ac
Sakithyan <sakithyanragavan@gmail.com>
2026-09-20 18:54:00 +0200

    Implement character_count()

--- textutils/transform.py
+++ textutils/transform.py
@@ -7,3 +7,11 @@ def word_count(text):
     """
    return len(text.split())
 
+def character_count(text):
+   """Count the number of characters in the given text.
+    Args:
+        text: The input string.
+    Returns:
+        The number of characters in the text.
+    """
+   return len(text)
