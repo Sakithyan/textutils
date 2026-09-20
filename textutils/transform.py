7542c8ed811b78e153b6f50521ecd68fe9a8ae73
Sakithyan <sakithyanragavan@gmail.com>
2026-09-20 18:48:41 +0200

    Implement word_count()

---
+++ textutils/transform.py
@@ -0,0 +1,9 @@
+def word_count(text):
+   """Count the number of words in the given text.
+    Args:
+        text: The input string.
+    Returns:
+        The number of words in the text.
+    """
+   return len(text.split())
+
