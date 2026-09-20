f31db1055a391a38beee14a22f970303aa3ac4a3
Sakithyan <sakithyanragavan@gmail.com>
2026-09-20 19:10:20 +0200

    Implement capitalize_words()

---
+++ textutils/casing.py
@@ -0,0 +1,9 @@
+def capitalize_words(text):
+    """
+    Capitalize the first letter of each word in the given text.
+    Args:
+        text: The input string.
+    Returns:
+        The string with each word capitalized.
+    """
+    return text.title()
